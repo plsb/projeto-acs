39fcfc3163363937b4fe52a0133c0c45d343c748
karloskelvinsantos <karloskelvinsantos@252b82af-1682-13e6-7f27-775d5c543e21>
2012-07-21 18:26:19 +0000

    Terminado tela de entrada e feita todas as activites que a tela inicial acessa. Feito tbm a tela de Familia

---
+++ ProjetoSCS/src/br/com/view/TelaAgendamento.java
@@ -0,0 +1,14 @@
+package br.com.view;
+
+import br.com.scs.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TelaAgendamento extends Activity{
+		
+	@Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.telaagendamento);
+	  }
+}
