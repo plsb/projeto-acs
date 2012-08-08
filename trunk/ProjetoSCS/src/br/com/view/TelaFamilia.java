70ad8903de6f1bbd1f2189d3b1c9034dd6ffe9d6
lucas.programacao@gmail.com <lucas.programacao@gmail.com@252b82af-1682-13e6-7f27-775d5c543e21>
2012-08-08 16:01:22 +0000

    Feito o xml e a classe da tela de login. [Lucas]

--- trunk/ProjetoSCS/src/br/com/view/TelaFamilia.java
+++ trunk/ProjetoSCS/src/br/com/view/TelaFamilia.java
@@ -2,15 +2,32 @@ package br.com.view;
 
 import br.com.scs.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class TelaFamilia extends Activity{
+public class TelaFamilia extends Activity implements OnClickListener{
+	
+	private Button btnIncluir, btnEditar;
 	
 	  @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
+	        
 	        setContentView(R.layout.telafamilia);
 	        
+	        btnIncluir = (Button) findViewById(R.telaFamilia.btnIncluir);
+	        btnIncluir.setOnClickListener(this);
+	        
+	  }
+	  
+	  public void onClick(View v) {
+		  Intent i = new Intent(this,TelaLogin.class);
+		  startActivity(i);
+		  //Toast.makeText(this, "Incluir", Toast.LENGTH_SHORT).show();
 	  }
 
 }
