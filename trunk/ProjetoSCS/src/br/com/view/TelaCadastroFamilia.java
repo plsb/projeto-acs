20e26338c2b00e0b91c570291ac7726853add2eb
karloskelvinsantos <karloskelvinsantos@252b82af-1682-13e6-7f27-775d5c543e21>
2012-10-09 14:38:08 +0000

    Implementação do EditText com DatePickerDialog na tela de cadastro de Familiares (Correçoes)

--- trunk/ProjetoSCS/src/br/com/view/TelaCadastroFamilia.java
+++ trunk/ProjetoSCS/src/br/com/view/TelaCadastroFamilia.java
@@ -31,6 +31,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
